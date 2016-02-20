5a0a795fe229b7ea4c3ae49933aeeaf9a133a53a
Paul Morgen <psmorgen@gmail.com>
2016-02-19 21:09:46 -0800

    Complete bonus section of calculator exercise

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -11,3 +11,23 @@ numbers = []
 def sum(numbers)
 numbers.inject(0) {|sum,n| sum+n}
 end
+
+def multiply(numbers)
+  numbers.inject {|product,n| product*n}
+end
+
+def power(first,second)
+  first**second
+end
+
+def factorial(number)
+  factors = Array.new
+  if number==0
+    return 0
+  else
+    for num in 1..number
+      factors.push(num)
+    end
+    factors.inject {|product,n| product*n}
+  end
+end
